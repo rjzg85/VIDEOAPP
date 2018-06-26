fe7c5d7445a160c68c6dad8e6e59bb16478198b7
Roberth Zambrano <rjzg85@gmail.com>
2018-06-25 20:52:17 -0500

    Initial Commit

---
+++ tabExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabExample
+//
+//  Created by Jessika Vivas on 6/25/18.
+//  Copyright © 2018 Roberth Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
